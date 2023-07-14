783d985c45898a185af7d162dec12e1d1bac5366
Mintu Sikder <mintu141617@gmail.com>
2023-07-14 11:33:51 +0600

    added icon

--- src/component/Product/Prodect.jsx
+++ src/component/Product/Prodect.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Prodect = (props) => {
-    console.log(props.product)
-    const {img,name,seller,quantity,price} = props.product
+  // console.log(props.handelAddToCard)
+//   console.log(props);
+  const { img, name, seller, price, ratings } = props.product;
+  const handelAddToCard = props.handelAddToCard;
 
-    return (
-        <div className='product'> 
-            <img src={img} alt="" />
-            <div className='prodact-info'>
-            <h6 className='product-name'>{name}</h6>
-            <p>${price}</p>
-            <p>Manufacturer :{seller}</p>
-            <p>Rating : {quantity}</p>
-            </div>
-            <button className='btn-card'>Add to Cart</button>
-        </div>
-    );
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="prodact-info">
+        <h6 className="product-name">{name}</h6>
+        <p>${price}</p>
+        <p>Manufacturer :{seller}</p>
+        <p>Rating : {ratings}</p>
+      </div>
+      <button
+        onClick={() => handelAddToCard(props.product)}
+        className="btn-card"
+      >
+        
+        Add to Cart
+        <FontAwesomeIcon icon={faCartShopping} />
+      </button>
+    </div>
+  );
 };
 
 export default Prodect;
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import Prodect from "../Product/Prodect";
 
 const Shop = () => {
   const [prodacts, setProdacts] = useState([]);
+  const [card, setCard] = useState([])
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
@@ -11,15 +12,23 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProdacts(data));
   }, []);
+  const handelAddToCard = (product) => {
+    const newCard = [...card, product]
+    setCard(newCard)
+  };
   return (
     <div className="shop-Container">
       <div className="prodact-container">
         {prodacts.map((product) => (
-          <Prodect key={product.id} product={product}></Prodect>
+          <Prodect
+            key={product.id}
+            product={product}
+            handelAddToCard={handelAddToCard}
+          ></Prodect>
         ))}
       </div>
       <div className="card-container">
-        <h4>Ordar Summary</h4>
+        <h4>Ordar Summary {card.length}</h4>
       </div>
     </div>
   );
