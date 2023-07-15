5ae7205368accb02d92f83d10c945c431281d306
Mintu Sikder <mintu141617@gmail.com>
2023-07-15 23:15:34 +0600

    added product quantity

--- src/component/Card/Cart.jsx
+++ src/component/Card/Cart.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import "./Cart.css";
-const Cart = ({card}) => {
-    // console.log(card)
-    let total =0
-    let totalShipping =0
-    for(const product of card){
-        total = total + product.price
-        totalShipping = totalShipping + product.shipping
-    }
-const tax = total*7/100
-const grandTotal = total + totalShipping + tax
+const Cart = ({ card }) => {
+  // console.log(card)
+  let total = 0;
+  let totalShipping = 0;
+  let quantity = 0;
+  for (const product of card) {
+    // if (product.quantity === 0) {
+    //   product.quantity = 1;
+    // }
+    // product.quantity = product.quantity || 1;
+    total = total + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping * product.quantity;
+    quantity = quantity + product.quantity;
+  }
+  const tax = (total * 7) / 100;
+  const grandTotal = total + totalShipping + tax;
   return (
     <div className="cart">
       <h4 className="cart-title">Order Summary</h4>
-      <p>Selected Items: {card.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax}</p>
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -14,9 +14,25 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProdacts(data));
   }, []);
-  useEffect(() =>{
-    const storedCart = getShoppingCart()
-  },[])
+useEffect(() =>{
+  const storedCard = getShoppingCart()
+  const savedCart = []
+  //step 1 get id to added product
+  for(const id in storedCard){
+    //step2 get product form products state by using id
+    const addedProduct = prodacts.find(product => product.id === id)
+    if(addedProduct){
+      //added quantity 
+      const quantity = storedCard[id]
+      addedProduct.quantity = quantity
+      // step 4 add the added product to the saved cart
+      savedCart.push(addedProduct)
+    }
+    // console.log('added product', addedProduct)
+  }
+  // set the cart
+  setCard(savedCart)
+},[prodacts])
   const handelAddToCard = (product) => {
     const newCard = [...card, product];
     setCard(newCard);
